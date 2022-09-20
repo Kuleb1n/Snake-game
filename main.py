5bdb68875f78550858b0aea0d7cab5e1d16ec522
Kuleb1n <v_vpyt@mail.ru>
2022-09-20 18:05:08 +0300

    The main game file

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import sys
+
 import pygame
 
 pygame.init()
+
+WHITE = (255, 255, 255)
+screen = pygame.display.set_mode((600, 500))
+pygame.display.set_caption('Snake')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill(WHITE)
+    pygame.display.update()
